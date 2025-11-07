efd76ffa25e56dc8747f24319ec431b574b694e1
ToniBirat7 <gotofreelancing1@gmail.com>
2025-11-07 23:17:52 +0545

    docs: backend setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,15 @@
 import express from "express";
 import routes from "./routes/index.js";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
 
 // Middlewares
 app.use(express.json());
