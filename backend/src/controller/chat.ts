86de9876942fd300e405c3d0ddf6dee153e82e0e
ToniBirat7 <gotofreelancing1@gmail.com>
2025-11-08 21:25:44 +0545

    update: jwt setup

--- backend/src/controller/chat.ts
+++ backend/src/controller/chat.ts
@@ -1,7 +1,18 @@
 // Related to chats
+import type { Request, Response } from "express";
 
 import { Conversation } from "../model/chat.model.js";
 
-export const saveConvo = (req: Request, res: Response) => {};
+export const saveConvo = (req: Request, res: Response) => {
+  res.json();
+};
+
+export const getConvo = (req: Request, res: Response) => {
+  res.json();
+};
 
 export const deleteConvo = (req: Request, res: Response) => {};
+
+export const editConvo = (req: Request, res: Response) => {
+  res.json();
+};
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,17 +1,25 @@
 // For Routes
 import { Router } from "express";
 import { createUser, loginUser } from "../controller/auth.js";
+import { authenticateJWT } from "../middleware/middleware.js";
+import {
+  saveConvo,
+  getConvo,
+  deleteConvo,
+  editConvo,
+} from "../controller/chat.js";
 
 //  Auth Router
 const authRouter = Router();
-
-// Middleware for Auth
-
-
-//  Auth ChatRouter
-const chatRouter = Router();
-
 authRouter.post("/create-user", createUser);
-authRouter.post("/login", loginUser);
+authRouter.post("/login", authenticateJWT, loginUser);
+
+//  Chat Router
+const chatRouter = Router();
+chatRouter.use(authenticateJWT);
+chatRouter.get("/get", getConvo);
+chatRouter.post("/save", saveConvo);
+chatRouter.post("/update", getConvo);
+chatRouter.post("/delete", deleteConvo);
 
 export { authRouter, chatRouter };
