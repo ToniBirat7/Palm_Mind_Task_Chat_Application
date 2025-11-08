0b36212d32ad2bda26d312cfaa2476dc17773ed3
ToniBirat7 <gotofreelancing1@gmail.com>
2025-11-08 13:01:37 +0545

    update: backend io

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -1,1 +1,9 @@
 // hook for components to access socket + status
+import { io } from "socket.io-client";
+
+const useSocket = () => {
+  const socket = io("http://localhost/3000");
+  return socket;
+};
+
+export default useSocket;
