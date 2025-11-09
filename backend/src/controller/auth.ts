b92cb5321b1c068b8bddcda3247b9805c6a6948d
ToniBirat7 <gotofreelancing1@gmail.com>
2025-11-09 07:53:16 +0545

    update: jwt setup

--- backend/src/controller/auth.ts
+++ backend/src/controller/auth.ts
@@ -1,6 +1,7 @@
 // Related to auths
 import type { Request, Response, NextFunction } from "express";
-// import { Jwt } from "jsonwebtoken";
+import { Jwt } from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 import { User } from "../model/chat.model.js";
 
@@ -13,11 +14,21 @@ export const createUser = async (req: Request, res: Response) => {
     const pwd = body.password;
   }
 
-  const user = await User.findOne({ email });
-  if (!user) return res.status(400).json({ msg: "User Does Not Exists" });
-  res.json({ msg: "User Saved" });
+  const isPasswordValid = await res.json({ msg: "User Saved" });
 };
 
-export const loginUser = (req: Request, res: Response) => {
+export const loginUser = async (req: Request, res: Response) => {
+  const body: UserPayload = req.body;
+
+  if (body) {
+    console.log("Body is : ", body);
+    const email = body.email;
+    const pwd = body.password;
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ msg: "User Does Not Exists" });
+
+  const isPasswordValid = await res.json({ msg: "User Saved" });
   res.send();
 };
