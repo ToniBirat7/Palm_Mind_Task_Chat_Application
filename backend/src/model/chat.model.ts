6ddae78ee40618702df12ce46ea997cea40950ca
ToniBirat7 <gotofreelancing1@gmail.com>
2025-11-09 08:26:48 +0545

    update: fronten login page

--- backend/src/model/chat.model.ts
+++ backend/src/model/chat.model.ts
@@ -1,6 +1,21 @@
 import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
-const userSchema = new Schema({});
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export const User = model("User", userSchema);
